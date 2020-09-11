8c471d569de72cbdaed98cbb34df5a1775a6db70
Austin Burton <abur3460@gmail.com>
2020-09-10 23:16:27 -0400

    small changes, update favicon

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 window.onload = function () {
+	$('#cs-one').spectrum({
+		color: '#6e1b55',
+		showInput: true,
+		showInitial: true,
+		showAlpha: false,
+		disabled: false,
+		showPalette: false,
+		clickoutFiresChange: true,
+		chooseText: 'Select',
+		showButtons: false,
+	});
+	$('#cs-two').spectrum({
+		color: '#4d8fcc',
+		showInput: true,
+		showInitial: true,
+		showAlpha: false,
+		disabled: false,
+		showPalette: false,
+		clickoutFiresChange: true,
+		chooseText: 'Select',
+		showButtons: false,
+	});
+	$('#cp-one').click(function () {
+		$('#cs-one').spectrum('toggle');
+		return false;
+	});
+	$('#cp-two').click(function () {
+		$('#cs-two').spectrum('toggle');
+		return false;
+	});
+
 	$('#gradient-bar').slider({
 		range: false,
 		animate: 'fast',
@@ -14,10 +45,11 @@ window.onload = function () {
 		// });
 	});
 };
+
 function setGradient(deg, color1, color2) {
 	console.log(color1, color2);
-	$('#cc-one').text(`#${color1}`);
-	$('#cc-two').text(`#${color2}`);
+	$('#cs-one').text(`#${color1}`);
+	$('#cs-two').text(`#${color2}`);
 	document.getElementById(
 		'canvas'
 	).style.backgroundImage = `linear-gradient(${deg}deg, #${color1}, #${color2})`;
