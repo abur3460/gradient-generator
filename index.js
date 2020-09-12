917bac5c3b7fc8e3fd846c07699817bbd715de1b
Austin Burton <abur3460@gmail.com>
2020-09-12 15:17:59 -0400

    simplify design, finished

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let degree = '90';
+let currentColor1 = '#f8757f';
+let currentColor2 = '#4cc7d4';
+
 window.onload = function () {
 	$('#cs-one').spectrum({
-		color: '#6e1b55',
-		showInput: true,
+		color: '#f8757f',
+		showInput: false,
 		showInitial: true,
 		showAlpha: false,
 		disabled: false,
@@ -11,8 +15,8 @@ window.onload = function () {
 		showButtons: false,
 	});
 	$('#cs-two').spectrum({
-		color: '#4d8fcc',
-		showInput: true,
+		color: '#4cc7d4',
+		showInput: false,
 		showInitial: true,
 		showAlpha: false,
 		disabled: false,
@@ -33,6 +37,7 @@ window.onload = function () {
 		var hexVal = tinyColor.toHexString();
 		var color_a = document.getElementById('cs-one');
 		color_a.style.backgroundColor = hexVal;
+		currentColor1 = color_a;
 		less.modifyVars({
 			'@color-1': `${hexVal}`,
 		});
@@ -43,64 +48,32 @@ window.onload = function () {
 		console.log(hexVal);
 		var color_a = document.getElementById('cs-two');
 		color_a.style.backgroundColor = hexVal;
+		currentColor2 = color_a;
 		less.modifyVars({
 			'@color-2': `${hexVal}`,
 		});
 		$('#cp-two').text(`${hexVal}`);
 	});
-	$('#gradient-bar').slider({
-		range: false,
-		animate: 'fast',
-		min: 0,
-		max: 100,
-		values: [0, 100],
-		orientation: 'horizontal',
-	});
-	$('#gradient-bar').on('slide', function (event, ui) {
-		// less.modifyVars({
-		// 	'@slider-val1': `${ui.values[1]}`,
-		// 	'@slider-val2': `${ui.values[0]}`,
-		// });
-	});
-	$('#slider').roundSlider({
-		min: 0,
-		max: 360,
-		step: 1,
-		radius: 60,
-		width: 12,
-		startAngle: 0,
-		endAngle: '+360',
-		animation: true,
-		showTooltip: true,
-		editableTooltip: true,
-		readOnly: false,
-		disabled: false,
-		keyboardAction: true,
-		mouseScrollAction: false,
-		circleShape: 'full',
-		lineCap: 'circle',
-		handleSize: 25,
-
-		change: 'traceEvent',
-		drag: 'traceEvent',
-	});
-
-	function traceEvent(e) {
-		less.modifyVars({
-			'@deg': `${e.value}deg`,
-		});
-	}
 };
+
+function setDegree(deg) {
+	let degree = `to ${deg}`;
+	less.modifyVars({
+		'@deg': `${degree}`,
+	});
+}
+
+function updateCurrent(deg, color1, color2) {
+	$('.current').text(`linear-gradient(${deg}, #${color1}, #${color2})`);
+}
 
 function setGradient(deg, color1, color2) {
 	console.log(color1, color2);
 	$('#cp-one').text(`#${color1}`);
 	$('#cp-two').text(`#${color2}`);
-	document.getElementById(
-		'canvas'
-	).style.backgroundImage = `linear-gradient(${deg}deg, #${color1}, #${color2})`;
 	less.modifyVars({
 		'@color-1': `#${color1}`,
 		'@color-2': `#${color2}`,
 	});
+	updateCurrent(deg, color1, color2);
 }
